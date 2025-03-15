c7ebcc453473b4ab8f3e0a3a48f96712535584a5
Radu Lupan <github@lupan.ca>
2025-03-14 21:32:57 -0400

    Changed instance type

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0005ee01bca55ab66"  # An example Amazon Linux 2 AMI for us-east-1
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
   tags = {
     Name = "GitHubActionsEC2Example"
   }
